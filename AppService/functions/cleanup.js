49fb6d88381159ce6af713e2d5ad9f1ed03130a6
timolackmann <Timo.Lackmann@mongodb.com>
2023-06-23 06:17:19 +0200

    clean-up function

--- AppService/functions/cleanup.js
+++ AppService/functions/cleanup.js
@@ -1,30 +1,15 @@
 exports = async function(){
-  // This default function will get a value and find a document in MongoDB
-  // To see plenty more examples of what you can do with functions see: 
-  // https://www.mongodb.com/docs/atlas/app-services/functions/
+  
+  // This function is triggered by a scheduled event and deletes all documents in the collection older than 30 days
 
-  // Find the name of the MongoDB service you want to use (see "Linked Data Sources" tab)
+  // Get the collection object
   var serviceName = "mongodb-atlas";
-
-  // Update these to reflect your db/collection
-  var dbName = "db_name";
-  var collName = "coll_name";
-
-  // Get a collection from the context
+  var dbName = "mqlConverter";
+  var collName = "queries";
   var collection = context.services.get(serviceName).db(dbName).collection(collName);
 
-  var findResult;
   try {
-    // Get a value from the context (see "Values" tab)
-    // Update this to reflect your value's name.
-    var valueName = "value_name";
-    var value = context.values.get(valueName);
-
-    // Execute a FindOne in MongoDB 
-    findResult = await collection.findOne(
-      { owner_id: context.user.id, "fieldName": value, "argField": arg},
-    );
-
+    var deleteResult = await collection.deleteMany({ "date": { "$lt": new Date(currentDate.setDate(currentDate.getDate() - 30)) } });
   } catch(err) {
     console.log("Error occurred while executing findOne:", err.message);
 
@@ -34,5 +19,5 @@ exports = async function(){
   // To call other named functions:
   // var result = context.functions.execute("function_name", arg1, arg2);
 
-  return { result: findResult };
+  return { result: deleteResult };
 };
